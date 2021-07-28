73d848e8553d6dc2136d2f084a3a90d5495fdcfc
claubatista <contatodaclau@gmail.com>
2021-07-28 19:18:20 +0200

    creates the TransactionTable component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Valor</th>
+            <th>categories</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Salario</td>
+            <td className="deposit">R$1.000,00</td>
+            <td>tabalho</td>
+            <td>05/10/2021</td>
+          </tr>
+          <tr>
+            <td>alugue</td>
+            <td className="withdraw">R$-500,00</td>
+            <td>tabalho</td>
+            <td>05/10/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
